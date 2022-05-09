c2e08ba43e1b382179c7640f20acf560a2b26a76
Yash Majithiya <jhdpco11039@gmail.com>
2022-05-09 18:54:00 +0530

    fix:image uploader

--- backend/Routers/Post.js
+++ backend/Routers/Post.js
@@ -3,17 +3,43 @@ const router = express.Router();
 const postModel = require("../models/Post");
 
 const multer  = require('multer')
-const upload = multer({ dest: './uploads/' })
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+// var upload = multer({ storage: storage })
 
+const imageUpload = multer({
+  storage,
+  // limits: {
+  //    fileSize: 1000000 * 5, // 1000000 Bytes = 1 MB * 5
+  // },
+  fileFilter(req, file, cb) {
+      if (!/^image\/(jpeg|png|gif)$/.test(file.mimetype)) {
+          // upload only png and jpg format
+          return cb(
+              new Error(
+                  'Please upload an Image with ".jpg", ".jpeg", ".png" or ".gif" formats.'
+              )
+          );
+      }
+      cb(undefined, true);
+  },
+});
 
-router.post("/", upload.single('image'), async (req, res) => {
-  debugger
+router.post("/", imageUpload.single('image'), async (req, res) => {
+  // debugger
+  console.log("file_name", req.file);
   const model = new postModel({
     title: req.body.title,
     description: req.body.description,
     tags: req.body.tags,
-    user: req.body.user,
-    image: req.file.path
+    user: JSON.parse(req.body.user),
+    image: `uploads/${req.file.originalname}`
   });
 
   model.save()
--- backend/temp/server.js
+++ backend/temp/server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/api", router);
-app.use("/uploads", express.static(path.join(__dirname, "/../uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 app.use(express.static(path.join(__dirname, "/../frontend/build")));
 
 app.get("*", (req, res) => {
