87363753f440608d2797eb269068033c451b125d
Yash Majithiya <jhdpco11039@gmail.com>
2022-05-13 01:33:18 +0530

    - fix

--- backend/Routers/Question.js
+++ backend/Routers/Question.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
 // const mongoose = require('mongoose')
-const QuestionDB = require("../models/Question");
+const QuestionDB = require("../Models/Question");
 
 router.post("/", async (req, res) => {
   const questionData = new QuestionDB({
